8b83ca939c14edeab56d5da7982dbb1a10f23047
raferti <raferti712@yandex.ru>
2019-03-02 19:38:29 +0300

    Add models.py Add table in data_base: User, Post Add context_processor

--- microblog.py
+++ microblog.py
@@ -1,6 +1,16 @@
 # импортируем переменную app из пакета(папки на верхнем уровне) "app"
-from app import app
+from app import app, dbm
+from app.models import User, post
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+# Декоратор app.shell_context_processor регистрирует функцию
+# как функцию контекста оболочки. Когда запускается команда flask shell,
+# она будет вызывать эту функцию и регистрировать элементы,
+# возвращаемые ею в сеансе оболочки.
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
